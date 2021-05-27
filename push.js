2de61072d3ab62869cdf9933d5890a35d18ad715
Simon Staszkiewicz <simon@staszkiewicz.ca>
2021-05-27 09:09:08 -0700

    Add git commit version storage

--- push.js
+++ push.js
@@ -6,6 +6,7 @@ const htmlFileName = process.env.BUCKET_NAME || "SimonStaszkiewiczResume.html";
 const zipName = process.env.ZIP_NAME || "SimonStaszkiewiczResume.zip";
 const region = process.env.BUCKET_REGION || 'us-west-2';
 const bucket = process.env.BUCKET_NAME || 'simon-staszkiewicz-public';
+const gitCommit = process.env.GIT_COMMIT;
 const s3 = new AWS.S3({region});
 
 async function push() {
@@ -13,33 +14,39 @@ async function push() {
     zip.addLocalFile(docFileName);
     zip.addLocalFile(htmlFileName);
     zip.toBuffer(async zipBuffer => {
-        const baseParams = {
-            Bucket: bucket,
-            ACL: 'public-read',
-        }
-        await Promise.all([
-            s3.putObject({
-                ...baseParams,
-                Key: zipName,
-                Body: zipBuffer
-            }).promise(),
+        let promises = [
+            pushToS3(zipName, zipBuffer),
+            pushToS3(docFileName, fs.readFileSync(docFileName)),
+            pushToS3(htmlFileName, fs.readFileSync(htmlFileName)),
+        ]
 
-            s3.putObject({
-                ...baseParams,
-                Key: docFileName,
-                Body: fs.readFileSync(docFileName)
-            }).promise(),
-            
-            s3.putObject({
-                ...baseParams,
-                Key: htmlFileName,
-                Body: fs.readFileSync(htmlFileName)
-            }).promise(),
-        ]).catch(e => {
+        if (gitCommit) {
+            promises = [
+                ...promises,
+                pushToS3(`${gitCommit}/${zipName}`, zipBuffer),
+                pushToS3(`${gitCommit}/${docFileName}`, fs.readFileSync(docFileName)),
+                pushToS3(`${gitCommit}/${htmlFileName}`, fs.readFileSync(htmlFileName)),
+            ]
+        }
+
+        await Promise.all(promises)
+        .then(result => {
+            console.log('Successfully pushed all documents to S3.')
+        })
+        .catch(e => {
             console.error(e)
             process.exit(1)
         });
     });
+}
+
+function pushToS3(key, body) {
+    return s3.putObject({
+        Bucket: bucket,
+        ACL: 'public-read',
+        Key: key,
+        Body: body,
+    }).promise()
 }
 
 push()
--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+export GIT_COMMIT=$(git rev-parse HEAD)
 npm run push
